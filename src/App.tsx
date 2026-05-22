e26a9e160208b9f57316049142625c7ed49d4b62
Ayush Karma <ayushkarma.dev@gmail.com>
2026-05-22 17:07:16 +0530

    feat: add mouse parallax effect to background dot pattern

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,20 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Mainlayout from "./components/layout/Mainlayout";
 import HomePage from "./pages/HomePage";
 import NotFound from "./pages/NotFound";
+import { useEffect } from "react";
 
 const App = () => {
+  useEffect(() => {
+    const handleMouseMove = (e: MouseEvent) => {
+      const moveX = (e.clientX - window.innerWidth / 2) * 0.01;
+      const moveY = (e.clientY - window.innerHeight / 2) * 0.01;
+      document.body.style.backgroundPosition = `${moveX}px ${moveY}px, ${moveX}px ${moveY}px, ${moveX}px ${moveY}px`;
+    };
+
+    window.addEventListener("mousemove", handleMouseMove);
+    return () => window.removeEventListener("mousemove", handleMouseMove);
+  }, []);
+
   return (
     <>
       <BrowserRouter>
